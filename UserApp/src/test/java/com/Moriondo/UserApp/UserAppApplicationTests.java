9dc075b45ccf81b1c7eac08a3c978a0cf5bb6d8c
lfelipemaf <61876877+lfelipemaf@users.noreply.github.com>
2021-06-20 07:29:57 -0300

    User App

---
+++ UserApp/src/test/java/com/Moriondo/UserApp/UserAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Moriondo.UserApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
